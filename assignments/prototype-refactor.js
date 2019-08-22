f7f374ad8e22dc659ae63e44b8fafc3c3579ded3
Alison Ludick <alison.ludick@gmail.com>
2019-08-22 14:02:09 +0200

    first commit to refactor prototype

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,18 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    // this.greet = function() {
+    //     return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    // }
+};
+
+Person.prototype.greet = function() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+}
+
+const Alison = new Person('Alison', 21);
+console.log(Alison.greet());
